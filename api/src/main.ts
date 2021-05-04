a9d9d2fbc63f6afebc85e882871b7cfa8696ba57
Roman <hmrfins@gmail.com>
2021-05-05 00:38:59 +0300

    same

--- api/src/main.ts
+++ api/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
