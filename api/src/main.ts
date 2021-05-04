7aa39869aeffece9f00cd10a18b4c918d8b568f9
Roman <hmrfins@gmail.com>
2021-05-05 00:18:37 +0300

    testing api with localhost orgin

--- api/src/main.ts
+++ api/src/main.ts
@@ -16,17 +16,9 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(cookieParser());
-  app.use(session({
-    secret: 'secretKey',
-    cookie: {
-      path: '/',
-      domain: 'magic-music-library.herokuapp.com',
-      maxAge: 1000 * 60 * 24 // 24 hours
-    }
-  }));
   app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
