25a4eb4ac0cb657a600b7f17230ee97c603499f5
Roman <hmrfins@gmail.com>
2021-05-07 05:32:42 +0300

    deleted api folder

--- api/src/auth/roles/roles.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { SetMetadata, applyDecorators } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
-
-// @Roles('admin')
-
-export const Roles = (...roles: string[]) => applyDecorators(
-    SetMetadata('roles', ['root', ...roles]),
-    ApiOperation({summary: `(${roles} only)`})
-);
