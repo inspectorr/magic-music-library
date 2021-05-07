e4b3e2d089681ff3b3a6c5781ce7a3af95f277bb
Roman <hmrfins@gmail.com>
2021-05-07 03:31:07 +0300

    overriding env on start

--- api/src/database/orm.config.ts
+++ api/src/database/orm.config.ts
@@ -1,20 +1,6 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { UserEntity } from '@/users/model/user.entity';
 
-try {
-    const fs = require('fs');
-    const localEnvFile = fs.readFileSync(`.env.local`);
-    if (localEnvFile) {
-        const dotenv = require('dotenv');
-        const envConfig = dotenv.parse(localEnvFile);
-        for (const key in envConfig) {
-            process.env[key] = envConfig[key];
-        }
-    }
-} catch (e) {
-    console.info('No local .env.local file used.');
-}
-
 function getMigrationDirectory() {
     const directory = process.env.NODE_ENV === 'migration' ? 'src' : `${__dirname}`;
     return `${directory}/database/migrations/**/*{.ts,.js}`;
--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,20 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from '@/app.module';
 import * as cookieParser from 'cookie-parser';
 
+try {
+  const fs = require('fs');
+  const localEnvFile = fs.readFileSync(`.env.local`);
+  if (localEnvFile) {
+    const dotenv = require('dotenv');
+    const envConfig = dotenv.parse(localEnvFile);
+    for (const key in envConfig) {
+      process.env[key] = envConfig[key];
+    }
+  }
+} catch (e) {
+  console.info('No local .env.local file used.');
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
