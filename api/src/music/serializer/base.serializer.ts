2496949c421a35ff00613c4b81742b9482c15849
Roman <hmrfins@gmail.com>
2021-05-30 05:01:17 +0300

    removed depr seializers

--- api/src/music/serializer/base.serializer.ts
+++
@@ -1,3 +0,0 @@
-export class BaseSerializer {
-    id;
-}
--- api/src/music/serializer/song.serializer.ts
+++
@@ -1,9 +0,0 @@
-import { ArtistBandSerializer } from './artist-band.serializer';
-
-export class SongSerializer extends ArtistBandSerializer {
-    name;
-    releasedAt;
-    genres;
-    album;
-    albumOrder;
-}
